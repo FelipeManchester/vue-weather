6f85934fc4a0ae06cff227744714070618cadda9
Felipe Manchester <f.p2809@gmail.com>
2023-03-12 19:26:57 -0300

    fix temperature

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" :class="typeof weather.main != 'undefined' ? (weather.main.temp > 30 ? 'hot' : (weather.main.temp > 20 ? 'warm' : 'cold')) : ''">
+  <div id="app" :class="typeof weather.main != 'undefined' ? (weather.main.temp > 29 ? 'hot' : (weather.main.temp > 16 ? 'warm' : 'cold')) : ''">
     <main>
       <div class="search-box">
         <input 
